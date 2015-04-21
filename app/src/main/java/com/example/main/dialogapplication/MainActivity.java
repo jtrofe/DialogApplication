19c37339ca45d77ecb626f5aeadb79fdbe2bc2a8
jtrofe <jamestrofe@gmail.com>
2015-04-21 17:20:11 -0400

    StuffS

--- app/src/main/java/com/example/main/dialogapplication/MainActivity.java
+++ app/src/main/java/com/example/main/dialogapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.main.dialogapplication;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RatingBar;
 
 import com.example.main.dialogapplication.customDialogs.LogInDialog;
 import com.example.main.dialogapplication.customDialogs.RateDialog;
@@ -12,10 +14,21 @@ import com.example.main.dialogapplication.customDialogs.RatingInterface;
 public class MainActivity extends Activity implements RatingInterface{
     private float mRating = -1;
 
+    private RatingBar stars;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayout layout = (LinearLayout) findViewById(R.id.starLayout);
+        stars = (RatingBar) findViewById(R.id.stars);
+        layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RateClick(v);
+            }
+        });
     }
 
     public void RateClick(View v){
@@ -28,5 +41,6 @@ public class MainActivity extends Activity implements RatingInterface{
 
     public void OnRatingDone(float rating){
         mRating = rating;
+        stars.setRating(rating);
     }
 }
